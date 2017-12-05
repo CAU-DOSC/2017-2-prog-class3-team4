5bd93ec6df5052ba546c08b4dee0c964a357f014
holquew <holquew@gmail.com>
2017-12-05 13:40:04 +0900

    Modify Juggling by adding null character

--- Project 2/Juggling_Algo.c
+++ Project 2/Juggling_Algo.c
@@ -7,19 +7,19 @@
 int gcd(int m, int n) {
 	return n ? gcd(n, m%n) : m;
 }
-char* Juggling(char arr[], int n, int k) {
+char* Juggling(char arr[], int n, int k) {	//인자로 전달되는 n의 크기는 문자열 자체의 크기(널문자 포함안됨)
 	int d = -1, i, j, temp;
-	char* dest = (int*)malloc((sizeof(char))*n);
+	char* dest = (int*)malloc((sizeof(char))*(n+1));
 	for (int i = 0; i < n; i++) {
 		dest[i] = arr[i];
 	}
+	dest[n] = '\0';
 	for (i = 0; i < gcd(n, k); i++) {
 		j = i;
 		temp = dest[i];
 		while (1) {
 			d = (j + k) % n;
 			if (d == i) break;
-
 			dest[j] = dest[d];
 			j = d;
 		}
